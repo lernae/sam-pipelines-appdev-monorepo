ae5f82a862f3411861af0224c78d0b36d2ed37ae
Lerna Ekmekcioglu <elerna@amazon.com>
2022-06-07 09:18:03 -0400

    trigger pipeline

--- projectB/hello_world/app.py
+++ projectB/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world b",
+            "message": "hello world x",
             # "location": ip.text.replace("\n", "")
         }),
     }
