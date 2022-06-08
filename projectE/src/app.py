4c8b4397c08836c0fc31ed5ce2b7bd82e4c40e53
Lerna Ekmekcioglu <elerna@amazon.com>
2022-06-08 11:07:19 -0400

    add a new project

--- projectE/src/app.py
+++ projectE/src/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world 1233",
+            "message": "hello world 123",
             # "location": ip.text.replace("\n", "")
         }),
     }
