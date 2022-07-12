c27ef81cf472d1e880039bb24305cb64a9d0fef7
Lerna Ekmekcioglu <elerna@amazon.com>
2022-07-11 20:23:16 -0400

    testing mult changes in one commit

--- projectABB/src/app.py
+++ projectABB/src/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world 2",
+            "message": "hello world 223232",
             # "location": ip.text.replace("\n", "")
         }),
     }
